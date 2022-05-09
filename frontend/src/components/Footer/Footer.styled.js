137e75088ab739de3df424538034bc9dfe2792f9
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-05-09 19:42:28 +0100

    import styles

--- frontend/src/components/Footer/Footer.styled.js
+++ frontend/src/components/Footer/Footer.styled.js
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 export const PageFooter = styled.footer`
   margin-top: auto;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
   text-align: center;
+  padding-top: 1.5rem;
+`;
+
+export const Copyright = styled.small`
+  display: block;
+`;
+
+export const CopyrightLink = styled.a`
+  color: #222;
 `;
