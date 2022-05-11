e97c726dc3317f2fb3295fb5fcd9f8d05934134a
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-05-11 17:18:44 +0100

    add loading and reverse res data

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,40 +1,39 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Section, Image, List, Figure, Location, Username, IconWrapper, Title } from "./Home.styled";
+import { Section, List, Figure, Location, Username, IconWrapper, Title } from "./Home.styled";
 import ModalImage from "react-modal-image";
 import { GoLocation } from "react-icons/go";
 import { AiOutlineUser } from "react-icons/ai";
 
 function Home() {
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("/api/images")
       .then((res) => {
-        console.log(res.data);
-        setImages(res.data);
+        setImages(res.data.reverse());
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  if (loading) {
+    return (
+      <Section>
+        <h2>Loading...</h2>
+      </Section>
+    );
+  }
+
   return (
     <Section>
       <Title>All Images</Title>
       <List>
         {images.map((image) => (
-          // <li key={image.cloudinary_id}>
-          //   <figure>
-          //     <Image src={image.image} alt={image.caption} data-testid="image" />
-          //     <figcaption>
-          //       <p>{image.caption}</p>
-          //       <p>{image.imageLocation}</p>
-          //     </figcaption>
-          //   </figure>
-          // </li>
-
           <li key={image.cloudinary_id}>
             <Figure>
               <div>
