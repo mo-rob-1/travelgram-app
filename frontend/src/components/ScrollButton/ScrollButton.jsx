45ac07adb8034b82a281315f8da98cf0a8812a18
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-05-11 17:10:38 +0100

    add scroll button

---
+++ frontend/src/components/ScrollButton/ScrollButton.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button } from "./ScrollButton.styled";
+import { BsArrowUp } from "react-icons/bs";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <Button>
+      <BsArrowUp
+        onClick={scrollToTop}
+        style={{
+          display: visible ? "inline" : "none",
+          background: "#222",
+          borderRadius: "50%",
+          padding: "0.7rem",
+        }}
+      />
+    </Button>
+  );
+};
+
+export default ScrollButton;
---
+++ frontend/src/components/ScrollButton/ScrollButton.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.div`
+  position: fixed;
+  right: 0;
+  bottom: 40px;
+  height: 20px;
+  font-size: 3rem;
+  margin-right: 2.5rem;
+  margin-bottom: 1.5rem;
+  z-index: 1;
+  cursor: pointer;
+  color: #fff;
+`;
