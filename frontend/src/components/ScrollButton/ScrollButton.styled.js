06b00e110f338b319a68510072835a145d08f64d
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-05-12 16:19:37 +0100

    remove margins

--- frontend/src/components/ScrollButton/ScrollButton.styled.js
+++ frontend/src/components/ScrollButton/ScrollButton.styled.js
@@ -2,13 +2,15 @@ import styled from "styled-components";
 
 export const Button = styled.div`
   position: fixed;
-  right: 0;
+  right: 1rem;
   bottom: 40px;
   height: 20px;
   font-size: 3rem;
-  margin-right: 2.5rem;
-  margin-bottom: 1.5rem;
   z-index: 1;
   cursor: pointer;
   color: #fff;
+  @media (min-width: 768px) {
+    bottom: 50px;
+    right: 2rem;
+  }
 `;
