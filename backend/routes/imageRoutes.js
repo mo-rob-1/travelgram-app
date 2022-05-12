ad13f2bff1ccd12bb5c6695c1e4463bf248e05a5
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-05-12 15:18:02 +0100

    remove unused route

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getImages, updateImage, deleteImage, uploadImage, getUserImages } = require("../controllers/imageController");
+const { getImages, deleteImage, uploadImage, getUserImages } = require("../controllers/imageController");
 const { protect } = require("../middleware/authMiddleware");
 const upload = require("../utils/multer");
 
 router.post("/", upload.single("image"), protect, uploadImage);
 router.route("/").get(getImages);
-router.route("/:id").put(protect, updateImage);
 router.route("/:id").delete(protect, deleteImage);
 router.route("/:id").get(getUserImages);
 
